fc7de166ce8d5a4f3bf768ad1946c86c31983f45
Alisaa2x <121780424+Alisaa2x@users.noreply.github.com>
2025-11-09 19:28:57 -0500

    Time: 0 ms (100.00%) | Memory: 19 MB (74.70%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,23 +1,31 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        left = self.bnSearch(nums, target, True)
-        right = self.bnSearch(nums, target, False)
-        return [left, right]
+        res = [-1, -1]
+        if not nums:
+            return res
 
-    def bnSearch(self, nums, target, leftBias):
+        # Find leftmost index
         l, r = 0, len(nums) - 1
-        i = -1
-
         while l <= r:
             m = (l + r) // 2
-            if target > nums[m]:
+            if nums[m] < target:
                 l = m + 1
-            elif target < nums[m]:
-                r = m - 1
             else:
-                i = m
-                if leftBias:
-                    r = m - 1
-                else:
-                    l = m + 1
-        return i
+                r = m - 1
+        if l < len(nums) and nums[l] == target:
+            res[0] = l
+        else:
+            return res  # target not found at all
+
+        # Find rightmost index
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] <= target:
+                l = m + 1
+            else:
+                r = m - 1
+        res[1] = r
+
+        return res
+# Time: O(2*log(n)) & Space: (1)
